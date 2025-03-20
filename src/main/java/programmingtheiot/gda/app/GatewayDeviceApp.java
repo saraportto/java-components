841e73f177bf1b40197f20828fe0a187ca9d34a3
saraporto <saraportoalvarez@gmail.com>
2025-03-20 20:10:58 +0100

    changes in GatewayDeviceApp

--- src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
+++ src/main/java/programmingtheiot/gda/app/GatewayDeviceApp.java
@@ -109,21 +109,6 @@ public class GatewayDeviceApp
 	{
 		_Logger.info("Starting GDA...");
 		
-		/*
-		try {
-			if (this.sysPerfMgr.startManager()) {
-				_Logger.info("GDA started successfully.");
-			} else {
-				_Logger.warning("Failed to start syster performance manager!");
-			}
-			stopApp(-1);
-		
-		} catch (Exception e) {
-			_Logger.log(Level.SEVERE, "Failed to start GDA. Exiting.", e);
-			stopApp(-1);
-		}
-		*/
-
 		try {
 			if (!ConfigUtil.getInstance().getBoolean(ConfigConst.GATEWAY_DEVICE,ConfigConst.TEST_EMPTY_APP_KEY)) {
 				this.dataMgr =new DeviceDataManager();
@@ -149,22 +134,7 @@ public class GatewayDeviceApp
 	public void stopApp(int code)
 	{
 		_Logger.info("Stopping GDA...");
-		
-		/*
-		try {
-			if (this.sysPerfMgr.stopManager()) {
-				_Logger.log(Level.INFO, "GDA stopped successfully with exit code {0}.", code);
-			} else {
-				_Logger.warning("Failed to stop syster performance manager!");
-			}
-			
-		} catch (Exception e) {
-			_Logger.log(Level.SEVERE, "Failed to cleanly stop GDA. Exiting.", e);
-		}
-
-		System.exit(code);
-		*/
-		
+	
 		try {
 			if (this.dataMgr !=null) {
 			this.dataMgr.stopManager();
